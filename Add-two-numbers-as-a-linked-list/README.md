68f189da3edc22a4aa0d51aa39698ef07f2b2aee
Rafael Ahrons <rafael.ahrons@gmail.com>
2019-10-17 19:52:57 -0300

    First commit

---
+++ Add-two-numbers-as-a-linked-list/README.md
@@ -0,0 +1,10 @@
+# Add two numbers as a linked list
+
+You have two linked lists that contains non-negative integers storeged in reverse orde and each node contains one digit.
+Example:
+
+```
+Input: [2, 4, 3] + [5, 6, 4]
+Output: [7, 0, 8]
+Explanation: 342 + 465 = 807
+```
---
+++ Add-two-numbers-as-a-linked-list/solution.py
@@ -0,0 +1,45 @@
+class ListNode(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, list1, list2):
+        converted1 = self.getConvertedValue(list1)
+        converted2 = self.getConvertedValue(list2)
+        
+        sum = converted1 + converted2
+        sumList = None
+        for digit in str(sum)[::-1]:
+            if sumList is None:
+                sumList = ListNode(digit)
+            else:
+                node = sumList
+                while node.next is not None:
+                    node = node.next
+                node.next = ListNode(digit)
+        
+        return sumList
+
+    def getConvertedValue(self, list):
+        value = ""
+        while list:
+            value += str(list.value)
+            list = list.next
+        
+        return int(value[::-1])
+
+list1 = ListNode(2)
+list1.next = ListNode(4)
+list1.next.next = ListNode(3)
+
+list2 = ListNode(5)
+list2.next = ListNode(6)
+list2.next.next = ListNode(4)
+
+result = Solution().addTwoNumbers(list1,list2)
+while result:
+    print(result.value)
+    result = result.next
+
+
