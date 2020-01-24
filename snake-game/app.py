1b9c15f1091a2bc0837933c43be5cee17a546e96
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-24 15:16:06 -0300

    Added print screen and translate values from screen matrix

--- snake-game/app.py
+++ snake-game/app.py
@@ -9,20 +9,44 @@ class SnakeGame(object):
         screen = [[0 for x in range(column_count)] for y in range(rows_count)]
 
         #get the rows
-        for i in range(len(screen)):
-            if i == 0 or i == len(screen) - 1:
-                screen[i] = self.add_same_value_entire_list(screen[i], 1)
+        for row in range(len(screen)):
+            if row == 0 or row == len(screen) - 1:
+                screen[row] = self.add_same_value_entire_list(screen[row], 1)
+            else:
+                for column in range(len(screen[row])):
+                    if column == 0 or column == len(screen[row]) - 1:
+                        screen[row][column] = 1        
 
         return screen
-
-
-
 
     def add_same_value_entire_list(self, list, value):
         populatedList = [value for i in range(len(list))]
 
         return populatedList
 
-     
+    def print_screen(self):
+        screen_text = ''
+        for row in self.screen:
+            for column in row:
+                screen_text = screen_text + self.translated_value(column)
+
+            screen_text = screen_text + '\n'
+        return screen_text
+
+    def translated_value(self, value):
+        if value == 1:
+            return '#'
+        elif value == 2:
+            return '*'        
+        elif value == -1: #snake body
+            return 'O'
+        elif value == -2: #runing snake head left <----
+            return '<'
+        elif value == -3: #runing  snake head right ---->
+            return '>'
+        else:
+            return ' '
+
+
 snake = SnakeGame(8, 8)
-print(snake.screen)
+print(snake.print_screen())
